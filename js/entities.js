77770cb387af60766797b492f0620afdff430a93
Chris Goodwin <chris@goodwinillustration.com>
2025-11-14 15:13:40 -0500

    Update entities.js

--- js/entities.js
+++ js/entities.js
@@ -14,29 +14,46 @@ export class Vector2 {
 }
 
 export class Piece {
+    // ... existing constructor ...
     constructor(pos, color, isTiger = false) {
         this.pos = pos;
         this.color = color;
         this.isTiger = isTiger;
         this.incapacitated = false;
+        this.isRemoved = false; // NEW: Permanently removed by camping
         this.radius = 15;
-        this.borderlandsTurns = 0; // NEW: Track consecutive camping turns
+        this.borderlandsTurns = 0;
     }
     
     draw(ctx) {
+        // NEW: Draw removed hunters as faded out
+        if (this.isRemoved) {
+            ctx.globalAlpha = 0.15;
+        }
+        
         const color = this.incapacitated ? '#333333' : this.color;
         ctx.fillStyle = color;
         ctx.beginPath();
         ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
         
-        if (this.incapacitated) {
+        if (this.isRemoved) {
+            // Draw removal mark (different from incapacitated)
+            ctx.globalAlpha = 0.5;
+            ctx.strokeStyle = '#95a5a6';
+            ctx.lineWidth = 2;
+            ctx.beginPath();
+            ctx.arc(this.pos.x, this.pos.y, this.radius + 5, 0, Math.PI * 2);
+            ctx.stroke();
+            ctx.globalAlpha = 1.0;
+        } else if (this.incapacitated) {
+            // Draw X for incapacitated
             ctx.strokeStyle = '#e74c3c';
             ctx.lineWidth = 3;
             ctx.beginPath();
-            ctx.moveTo(this.pos.x - 10, this.pos.y - 10); // FIXED: was cntx
+            ctx.moveTo(this.pos.x - 10, this.pos.y - 10);
             ctx.lineTo(this.pos.x + 10, this.pos.y + 10);
-            ctx.moveTo(this.pos.x - 10, this.pos.y + 10); // Added second leg of X
+            ctx.moveTo(this.pos.x - 10, this.pos.y + 10);
             ctx.lineTo(this.pos.x + 10, this.pos.y - 10);
             ctx.stroke();
         }
